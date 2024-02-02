926b410b428ff250933a2b040c24f4bd4e6a5de6
Shehab Ali Tweel <shehabtweel@gmail.com>
2024-02-02 14:37:02 +0200

    change

--- bittorrent-parser/src/lib/bittorrent-parser.spec.ts
+++
@@ -1,7 +0,0 @@
-import { bittorrentParser } from './bittorrent-parser';
-
-describe('bittorrentParser', () => {
-  it('should work', () => {
-    expect(bittorrentParser()).toEqual('bittorrent-parser');
-  });
-});
--- bittorrent-parser/src/lib/bittorrent-parser.ts
+++
@@ -1,3 +0,0 @@
-export function bittorrentParser(): string {
-  return 'bittorrent-parser';
-}
