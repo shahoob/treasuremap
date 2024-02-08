b97f6a57b5b5005d19a232a6cf85a73c1aebe8ec
Shehab Ali Tweel <shehabtweel@gmail.com>
2024-02-08 19:30:48 +0200

    remove extra projects
    thanks nx for putting the plugins and showing me everything now get rid of these projects

--- libs/test/test/src/lib/test-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { testTest } from './test-test';
-
-describe('testTest', () => {
-  it('should work', () => {
-    expect(testTest()).toEqual('test-test');
-  });
-});
--- libs/test/test/src/lib/test-test.ts
+++
@@ -1,3 +0,0 @@
-export function testTest(): string {
-  return 'test-test';
-}
